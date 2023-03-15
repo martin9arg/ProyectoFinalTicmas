502e41361ad1bf71fe2398d4ea061f9c6d47df4d
martin9arg <123436995+martin9arg@users.noreply.github.com>
2023-03-15 14:18:23 -0300

    Add files via upload

---
+++ proyecto.js
@@ -0,0 +1,22 @@
+function extra() { /**mostrar informacion extra con click */
+    
+        document.getElementById("informacionExtra").style.display = "block";
+
+}
+
+
+function agrandarFoto (x) {
+    x.style.width = "300px";
+    x.style.height = "300px";
+    
+    
+}
+
+function normalFoto(x) {
+    
+    x.style.width = "200px";
+    x.style.height = "200px";
+    
+}
+
+
